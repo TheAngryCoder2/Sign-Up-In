dd27f5496afaab2bafc5b6527a91e039464b6365
WhosRaph <79235452+WhosRaph@users.noreply.github.com>
2024-06-26 23:38:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let signUpBtn = document.getElementById("signUpBtn");
+let signIpBtn = document.getElementById("signIpBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+
+signIpBtn.addEventListener("click", () => {
+    
+})
